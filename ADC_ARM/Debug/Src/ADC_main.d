9aaa5bf96549a4adc0962c171c7b56ce3bd37c5b
muhsen sheta <78617731+Muhsensheta@users.noreply.github.com>
2023-03-22 07:01:59 +0200

    Add files via upload

---
+++ ADC_ARM/Debug/Src/ADC_main.d
@@ -0,0 +1,13 @@
+Src/ADC_main.o: ../Src/ADC_main.c ../Inc/STD_TYPES.h ../Inc/BIT_MATH.h \
+ ../Inc/RCC_interface.h ../Inc/DIO_int.h ../Inc/NVIC_int.h \
+ ../Inc/STK_interface.h ../Inc/ADC_init.h ../Inc/LCD_int.h \
+ ../Inc/SSD_init.h
+../Inc/STD_TYPES.h:
+../Inc/BIT_MATH.h:
+../Inc/RCC_interface.h:
+../Inc/DIO_int.h:
+../Inc/NVIC_int.h:
+../Inc/STK_interface.h:
+../Inc/ADC_init.h:
+../Inc/LCD_int.h:
+../Inc/SSD_init.h:
